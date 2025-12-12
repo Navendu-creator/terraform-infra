6fbdd11b2d0c1644aad35d1417f2f6eea16854e9
Navendu Dixit <navendu782@gmail.com>
2025-12-12 13:14:23 +0530

    added EKS module with acceptable instance type

--- terraform-infra-production/environments/dev/main.tf
+++ terraform-infra-production/environments/dev/main.tf
@@ -8,6 +8,7 @@ module "networking" {
   vpc_cidr        = var.vpc_cidr
   public_subnets  = var.public_subnets
   private_subnets = var.private_subnets
+  azs             = var.azs
   tags            = var.tags
 }
 
--- terraform-infra-production/environments/dev/variables.tf
+++ terraform-infra-production/environments/dev/variables.tf
@@ -21,7 +21,7 @@ variable "private_subnets" {
 
 variable "k8s_version" {
   type    = string
-  default = "1.29"
+  default = "1.33"
 }
 
 variable "node_groups" {
